5269176be72e08c79c0b6c839493373b031ae213
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-12-05 11:46:35 +0100

    Added data required for day-5 challenge

---
+++ day-5/rearrange-crates.js
@@ -0,0 +1,15 @@
+const rearrangementData = require('./data/rearrangement-data')
+
+const crateStacks = [
+  [V,C,D,R,Z,G,B,W],
+  [G,W,F,C,B,S,T,V],
+  [C,B,S,N,W],
+  [Q,G,M,N,J,V,C,P],
+  [T,S,L,F,D,H,B],
+  [J,V,T,W,M,N],
+  [P,F,L,C,S,T,G],
+  [B,D,Z],
+  [M,N,Z,W]
+]
+
+// Remove 'move' 'from' and 'to' from my data so I am left with 3 input numbers. 
