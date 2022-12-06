88933812a717b162b69ce6f2842f099c16b016b1
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-12-06 12:08:02 +0100

    Added solution to challenge one for day 6

---
+++ day-6/marker.js
@@ -0,0 +1,19 @@
+// Import data
+const markerCode = require("./data/marker-data");
+
+// Challenge one
+
+// Set up a new function that takes an array
+const findUnique = (arr) => {
+  // Loop through the array
+  for (let i = 0; i < arr.length; i++){
+    /* Slice out the next four elements in our array, starting from 0 and increasing by 1 
+    comparing if this slice of four elements is unique, if it is unique, 
+    return our iteration value + 4 */
+    if (new Set(arr.slice(i, i + 4)).size == arr.slice(i, i + 4).length) {
+      return console.log(i + 4)
+    };
+  }
+}
+
+findUnique(markerCode)
