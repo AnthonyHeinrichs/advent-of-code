4ba263c47db5ff243804be2b85bfa3ec30273876
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-12-06 12:14:22 +0100

    Added answer to second challenge

--- day-6/marker.js
+++ day-6/marker.js
@@ -4,16 +4,25 @@ const markerCode = require("./data/marker-data");
 // Challenge one
 
 // Set up a new function that takes an array
-const findUnique = (arr) => {
+const findUnique = (arr, num) => {
   // Loop through the array
   for (let i = 0; i < arr.length; i++) {
     /* Slice out the next four elements in our array, starting from 0 and increasing by 1 
     comparing if this slice of four elements is unique, if it is unique, 
     return our iteration value + 4 */
-    if (new Set(arr.slice(i, i + 4)).size == arr.slice(i, i + 4).length) {
-      return console.log(i + 4);
+    if (new Set(arr.slice(i, i + num)).size == arr.slice(i, i + num).length) {
+      return console.log(i + num);
     }
   }
 };
 
-findUnique("Solution to challenge one:", markerCode);
+console.log("Answer to first challenge:");
+findUnique(markerCode, 4);
+
+// Challenge two
+
+/* All I had to do for this challenge was update my above function to take in a unique 
+num that we reference for our unique char check */
+
+console.log("Answer to second challenge:");
+findUnique(markerCode, 14);
