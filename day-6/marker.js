e7a970e1a5fb07abd8649807cbe8c458bafe7003
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-12-06 12:08:59 +0100

    Updated formatting for challenge one

--- day-6/marker.js
+++ day-6/marker.js
@@ -6,14 +6,14 @@ const markerCode = require("./data/marker-data");
 // Set up a new function that takes an array
 const findUnique = (arr) => {
   // Loop through the array
-  for (let i = 0; i < arr.length; i++){
+  for (let i = 0; i < arr.length; i++) {
     /* Slice out the next four elements in our array, starting from 0 and increasing by 1 
     comparing if this slice of four elements is unique, if it is unique, 
     return our iteration value + 4 */
     if (new Set(arr.slice(i, i + 4)).size == arr.slice(i, i + 4).length) {
-      return console.log(i + 4)
-    };
+      return console.log(i + 4);
+    }
   }
-}
+};
 
-findUnique(markerCode)
+findUnique("Solution to challenge one:", markerCode);
