999e2587b6e1ffcb6d5db8976b7ad518f61d3eff
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-12-09 09:59:02 +0100

    Added solution for challenge 2 of day 9

--- 2022/day-9/rope.js
+++ 2022/day-9/rope.js
@@ -6,6 +6,8 @@ const moves = data
   .map((move) => move.split(" "))
   .map((move) => [move[0], parseInt(move[1])]);
 
+// Challenge one
+
 // Set the starting node position for our rope 'head'
 const head = [0, 0];
 // Set the starting node position for our rope 'tail'
@@ -65,5 +67,65 @@ for (const move of moves) {
     visitedNodes.add(JSON.stringify([tail[0], tail[1]]));
   }
 }
+
 // Since it is a Set, we must check 'size' instead of 'length' for our answer
 console.log("Answer to first challenge:", visitedNodes.size);
+
+// Challenge two
+
+let knots = [];
+
+/* Instead of tracking our head and tail individually, we will track rope with
+a length of 10 knots. Knot 1 is our head, knot 10 is our tail */
+for (let i = 0; i < 10; i++) {
+  knots.push([0, 0]);
+}
+
+const longRopeVisited = new Set();
+
+for (const move of moves) {
+  let moveDirection = possibleMoves[move[0]];
+
+  for (let i = 0; i < move[1]; i++) {
+    // Update our heads position by moving in our move direction
+    knots[0][0] += moveDirection[0];
+    knots[0][1] += moveDirection[1];
+
+    // Update the other 9 knots in our rope
+    for (let i = 1; i < 10; i++) {
+      // Grab head knot (hx, hy) and tail knot (tx, ty)
+      let hx = knots[i - 1][0];
+      let hy = knots[i - 1][1];
+      let tx = knots[i][0];
+      let ty = knots[i][1];
+
+      // If head and tail are not touching eachother, than we should update the tail
+      if (!isTouching(hx, hy, tx, ty)) {
+        let tailX = null;
+        let tailY = null;
+        // If the tail is already in the same column, we don't move rows
+        if (hx == tx) {
+          tailX = 0;
+          // Else, we need to move the column the difference between the head and the tail
+        } else {
+          tailX = (hx - tx) / Math.abs(hx - tx);
+        }
+        // If the tail is already in the same row, we don't move rows
+        if (hy == ty) {
+          tailY = 0;
+          // Else, we need to move the row the difference between the head and the tail
+        } else {
+          tailY = (hy - ty) / Math.abs(hy - ty);
+        }
+        // Update our current tail position
+        tx += tailX;
+        ty += tailY;
+        knots[i] = [tx, ty];
+      }
+    }
+    /* Add the last knot in our rope (tail) to our set*/
+    longRopeVisited.add(JSON.stringify(knots[9]));
+  }
+}
+
+console.log("Answer to second challenge:", longRopeVisited.size);
