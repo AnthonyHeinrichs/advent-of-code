7e9c2ff700ce2c0d4e462bc16ed9cc0d1b91188c
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-12-21 20:36:24 +0100

    Added solution for challenge 2 of day 21

--- 2022/day-21/monkey.js
+++ 2022/day-21/monkey.js
@@ -1,3 +1,9 @@
+// Require any dependencies
+const algebra = require("algebra.js");
+const { Equation } = require("algebra.js");
+const Expression = algebra.Expression;
+const math = require("mathjs");
+
 // Pull in our data from our text file and parse it
 const fs = require("fs");
 const path = require("path");
@@ -33,3 +39,36 @@ const findRoot = (line) => {
 };
 
 console.log("Answer to challenge one:", findRoot("root"));
+
+// Challenge two
+
+/* Recursive function that finds the final number for root by
+recursively passing the object name until a number is returned*/
+const findMatch = (line) => {
+  // If hit 'humn' just convert to an expression symbol (x), add to our exp string
+  if (line == "humn") {
+    let ex = new Expression("x");
+    return ex;
+  }
+  // If we get a number, return it
+  if (!isNaN(map[line])) {
+    return map[line];
+  }
+  // Getting our expressions for each line, left and right
+  let left = findMatch(map[line][0]);
+  let right = findMatch(map[line][2]);
+  // Using math.parse to concat our expression string
+  return math.parse(`(${left})${map[line][1]}(${right})`);
+};
+// Getting our expressions for left and right
+const left = findMatch(map["root"][0]).toString();
+const right = findMatch(map["root"][2]).toString();
+
+// Using algebra to parse the expressions
+const l = algebra.parse(left);
+const r = algebra.parse(right);
+// Creating a new equation (left math exp = right math expr)
+const eq = new Equation(l, r);
+// Finding our expression symbol ('x') and solving for it
+const solve = eq.solveFor("x");
+console.log("Answer to challenge two:", Math.ceil(solve.numer / solve.denom));
